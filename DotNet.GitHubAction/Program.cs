3c1001b2ff61b2ab4e20e7057db65a19c507d7b0
Dustuu <dustuu@furcade.com>
2023-02-01 23:21:41 +0800

    added dir walk

--- DotNet.GitHubAction/Program.cs
+++ DotNet.GitHubAction/Program.cs
@@ -42,7 +42,67 @@ static async Task TestGithubStuff(ActionInputs inputs, IHost host)
     Console.WriteLine($"Directory: {inputs.Directory}");
     Console.WriteLine($"WorkspaceDirectory: {inputs.WorkspaceDirectory}");
 
+    DirectoryInfo directory = new(inputs.Directory);
+    Console.WriteLine($"Testing: {directory.FullName}");
+    WalkDirectoryTree(directory);
+
+    DirectoryInfo workspaceDirectory = new(inputs.WorkspaceDirectory);
+    Console.WriteLine($"Testing: {workspaceDirectory.FullName}");
+    WalkDirectoryTree(workspaceDirectory);
+
+    Console.WriteLine("GOODBYE! :D");
+
     Environment.Exit(0);
+}
+
+// from https://learn.microsoft.com/en-US/dotnet/csharp/programming-guide/file-system/how-to-iterate-through-a-directory-tree
+static void WalkDirectoryTree(System.IO.DirectoryInfo root)
+{
+    Console.WriteLine($"WALKING DIR: {root.FullName}");
+
+    System.IO.FileInfo[] files = null!;
+    System.IO.DirectoryInfo[] subDirs = null!;
+
+    // First, process all the files directly under this folder
+    try
+    {
+        files = root.GetFiles("*.*");
+    }
+    // This is thrown if even one of the files requires permissions greater
+    // than the application provides.
+    catch (UnauthorizedAccessException e)
+    {
+        // This code just writes out the message and continues to recurse.
+        // You may decide to do something different here. For example, you
+        // can try to elevate your privileges and access the file again.
+        Console.WriteLine(e.Message);
+    }
+
+    catch (System.IO.DirectoryNotFoundException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+
+    if (files != null)
+    {
+        foreach (System.IO.FileInfo fi in files)
+        {
+            // In this example, we only access the existing FileInfo object. If we
+            // want to open, delete or modify the file, then
+            // a try-catch block is required here to handle the case
+            // where the file has been deleted since the call to TraverseTree().
+            Console.WriteLine(fi.FullName);
+        }
+
+        // Now find all the subdirectories under this directory.
+        subDirs = root.GetDirectories();
+
+        foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+        {
+            // Resursive call for each subdirectory.
+            WalkDirectoryTree(dirInfo);
+        }
+    }
 }
 
 var parser = Default.ParseArguments<ActionInputs>(() => new(), args);
