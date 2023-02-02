92ba0510cf2d703b23d4b09b4b9d75edd377f91d
Dustuu <dustuu@furcade.com>
2023-02-03 00:03:01 +0800

    clean up

--- DotNet.GitHubAction/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using CommandLine;
-global using DotNet.GitHubAction;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Hosting;
-global using Microsoft.Extensions.Logging;
-global using static CommandLine.Parser;
---
+++ Dustuu.Actions.BunnyCdnDeploy/ActionInputs.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace Dustuu.Actions.BunnyCdnDeploy;
+
+public class ActionInputs
+{
+    [Option('w', "workspace", Required = true)] public string Workspace { get; set; } = null!;
+
+    [Option('d', "dir", Required = true)] public string Directory { get; set; } = null!;
+
+    [Option('u', "bunny-cdn-username", Required = true)] public string BunnyCdnUsername { get; set; } = null!;
+
+    [Option('p', "bunny-cdn-password", Required = true)] public string BunnyCdnPassword { get; set; } = null!;
+
+    [Option('r', "bunny-cdn-region", Required = true)] public string BunnyCdnRegion { get; set; } = null!;
+
+    [Option('a', "bunny-cdn-api-key", Required = true)] public string BunnyCdnApiKey { get; set; } = null!;
+}
